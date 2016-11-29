235c5e0f234bcae7a4e7e10f5c5ac740024478e7
stefanie <xuchao1109@126.com>
2016-11-29 22:30:59 +0800

    fix bug

---
+++ src/main/java/test/model/UserInfo.java
@@ -0,0 +1,95 @@
+package test.model;
+
+import java.util.Date;
+
+public class UserInfo {
+    private Integer id;
+
+    private String password;
+
+    private String nickname;
+
+    private String mobile;
+
+    private String email;
+
+    private String iconpath;
+
+    private String gender;
+
+    private Date registdate;
+
+    private Boolean isForbidden;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname == null ? null : nickname.trim();
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile == null ? null : mobile.trim();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public String getIconpath() {
+        return iconpath;
+    }
+
+    public void setIconpath(String iconpath) {
+        this.iconpath = iconpath == null ? null : iconpath.trim();
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender == null ? null : gender.trim();
+    }
+
+    public Date getRegistdate() {
+        return registdate;
+    }
+
+    public void setRegistdate(Date registdate) {
+        this.registdate = registdate;
+    }
+
+    public Boolean getIsForbidden() {
+        return isForbidden;
+    }
+
+    public void setIsForbidden(Boolean isForbidden) {
+        this.isForbidden = isForbidden;
+    }
+}
--- src/test/dao/AboutDtoMapper.java
+++ src/test/dao/AboutDtoMapper.java
@@ -3,7 +3,15 @@ package test.dao;
 import test.model.AboutDto;
 
 public interface AboutDtoMapper {
+    int deleteByPrimaryKey(Integer id);
+
     int insert(AboutDto record);
 
     int insertSelective(AboutDto record);
+
+    AboutDto selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(AboutDto record);
+
+    int updateByPrimaryKey(AboutDto record);
 }
